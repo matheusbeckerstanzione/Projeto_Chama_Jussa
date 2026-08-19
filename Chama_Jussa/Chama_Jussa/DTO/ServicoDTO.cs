79e63e6f350ec1516363fdddee91e49cdf00ae9d
Joao-Santos-F <joaofeitosa029@gmail.com>
2026-08-19 10:49:28 -0300

    Finalizado a primeira parte da API

--- Chama_Jussa/Chama_Jussa/DTO/ServicoDTO.cs
+++ Chama_Jussa/Chama_Jussa/DTO/ServicoDTO.cs
@@ -2,8 +2,10 @@
 
 public class ServicoDTO
 {
+    public Guid? IdServico { get; set; }
     public string? Titulo { get; set; }
     public string? Maquina { get; set; }
+    public IFormFile? Imagem { get; set; }
     public string? Localizacao { get; set; }
     public string? Descricao { get; set; }
     public int? Numero_Servico { get; set; }
--- Chama_Jussa/Chama_Jussa/obj/Debug/net10.0/Chama_Jussa.AssemblyInfo.cs
+++ Chama_Jussa/Chama_Jussa/obj/Debug/net10.0/Chama_Jussa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chama_Jussa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc65640b1b89e2573bc8b3d6c079dc9923eec35d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928441c39c6df95a7a39bf1baa2998287ae74b3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chama_Jussa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chama_Jussa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
